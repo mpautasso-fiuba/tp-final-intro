81803c9632237c1c628a96b39a36c6dd41ebbe36
mpautasso-ubuntu <mpautasso@fi.uba.ar>
2025-07-13 12:06:05 -0300

    Se agrega script sql para insertar datos en la creacion de la bd

---
+++ base_de_datos/02_insert_data.sql
@@ -0,0 +1,21 @@
+--Inserts Generos
+INSERT INTO generos(nombre) VALUES('Aventura');
+INSERT INTO generos(nombre) VALUES('Accion');
+INSERT INTO generos(nombre) VALUES('Plataformas');
+INSERT INTO generos(nombre) VALUES('Lucha');
+INSERT INTO generos(nombre) VALUES('Metroidvania');
+INSERT INTO generos(nombre) VALUES('Roguelike');
+INSERT INTO generos(nombre) VALUES('Simulacion');
+INSERT INTO generos(nombre) VALUES('RPG');
+INSERT INTO generos(nombre) VALUES('Deportes');
+INSERT INTO generos(nombre) VALUES('Estrategia');
+
+--Inserts Plataformas
+INSERT INTO plataformas(nombre) VALUES('Xbox One');
+INSERT INTO plataformas(nombre) VALUES('Xbox Series X-S');
+INSERT INTO plataformas(nombre) VALUES('Steam');
+INSERT INTO plataformas(nombre) VALUES('Playstation 5');
+INSERT INTO plataformas(nombre) VALUES('Playstation 4');
+INSERT INTO plataformas(nombre) VALUES('Nintendo Switch');
+INSERT INTO plataformas(nombre) VALUES('Nintendo Switch 2');
+INSERT INTO plataformas(nombre) VALUES('Epic Games');
