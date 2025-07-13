e1ee0663a3f2eb587e17d68cb07114886993f0a3
mpautasso-ubuntu <mpautasso@fi.uba.ar>
2025-07-13 12:10:04 -0300

    Se agrega correccion de cors en Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const generosRoute = require('./routes/generos_api');
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/v1/health", (req, res) => {
   res.json({status: "OK"});
